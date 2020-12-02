cd7b60ad971359bf64d52493967597be90e05194
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-12-02 20:11:57 +0100

    feat: counter refactor to reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    default:
+      return state
+  }
+}
+
 const CounterScreen = () => {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, 0)
   return (
     <View>
-      <Button title='Inrcease' onPress={() => setCount(count + 1)} />
-      <Button title='Decrease' onPress={() => setCount(count - 1)} />
-      <Text>Current count: {count}</Text>
+      <Button
+        title='Inrcease'
+        onPress={() => dispatch({ type: 'increment' })}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => dispatch({ type: 'decrement' })}
+      />
+      <Text>Current count: {state}</Text>
     </View>
   )
 }
